2babf08bc793197e7756930054dd0ee23038a5f6
Alireza <alireza_paridar@outlook.com>
2016-03-07 15:29:07 +0330

    Start

---
+++ TagCompressor.Net/Compress.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text;
+using System.Web.Mvc;
+
+
+namespace TagCompressor.Net
+{
+    public class Compress : ActionFilterAttribute
+    {
+        private System.Web.UI.HtmlTextWriter tw;
+        private System.IO.StringWriter sw;
+        private StringBuilder sb;
+        private System.Web.HttpWriter output;
+
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            sb = new StringBuilder();
+            sw = new System.IO.StringWriter(sb);
+            tw = new System.Web.UI.HtmlTextWriter(sw);
+            output = (System.Web.HttpWriter)filterContext.RequestContext.HttpContext.Response.Output;
+            filterContext.RequestContext.HttpContext.Response.Output = tw;
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            string response = sb.ToString();
+            response = HtmlCompressor.Compress(response);
+            output.Write(response);
+        }
+
+    }
+}
---
+++ TagCompressor.Net/HtmlCompressor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace TagCompressor.Net
+{
+    class HtmlCompressor
+    {
+        public static string Compress(string html)
+        {
+            string pattern = @"\n+";
+            Regex r = new Regex(pattern);
+            string compressed = r.Replace(html, "");
+
+            pattern = @"\s+";
+            r = new Regex(pattern);
+            compressed = r.Replace(compressed, " ");
+
+
+            return compressed;
+        }
+    }
+}
